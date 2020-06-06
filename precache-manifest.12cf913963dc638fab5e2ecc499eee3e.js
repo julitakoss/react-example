e2266230bda0ee68259d14cf7d94b4b259e01339
julita_koss@tlen.pl <julitakoss@github.com>
2020-06-06 16:35:50 +0200

    Updates

---
+++ precache-manifest.12cf913963dc638fab5e2ecc499eee3e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1661389ac4de3dc189275450b1232625",
+    "url": "/react-example/index.html"
+  },
+  {
+    "revision": "2c1f66a4f4d6ec6253a0",
+    "url": "/react-example/static/css/main.cc4c7a5b.chunk.css"
+  },
+  {
+    "revision": "815afd56a4c4e735d356",
+    "url": "/react-example/static/js/2.28ac1ded.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-example/static/js/2.28ac1ded.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c1f66a4f4d6ec6253a0",
+    "url": "/react-example/static/js/main.bd8fc9bc.chunk.js"
+  },
+  {
+    "revision": "0565dc27099561815eb7",
+    "url": "/react-example/static/js/runtime-main.1f1ab67f.js"
+  }
+]);
